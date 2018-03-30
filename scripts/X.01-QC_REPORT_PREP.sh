077d6fd7e403d887c3cf3336f7231488644b5654
Kurt Hetrick <khetric1@jhmi.edu>
2018-03-29 20:11:03 -0400

    changed the freemix result in the qc report from a fraction to a percent

--- scripts/X.01-QC_REPORT_PREP.sh
+++ scripts/X.01-QC_REPORT_PREP.sh
@@ -99,7 +99,7 @@ fi
 ##### "VERIFYBAM_FREEMIX","VERIFYBAM_#SNPS","VERIFYBAM_FREELK1","VERIFYBAM_FREELK0","VERIFYBAM_DIFF_LK0_LK1","VERIFYBAM_AVG_DP" #####
 #####################################################################################################################################
 
-awk 'BEGIN {OFS="\t"} NR>1 {print $7,$4,$8,$9,($9-$8),$6}' \
+awk 'BEGIN {OFS="\t"} NR>1 {print $7*100,$4,$8,$9,($9-$8),$6}' \
 $CORE_PATH/$PROJECT/REPORTS/VERIFYBAMID/$SM_TAG".selfSM" \
 | $DATAMASH_DIR/datamash transpose \
 >> $CORE_PATH/$PROJECT/TEMP/$SM_TAG".QC_REPORT_TEMP.txt"
