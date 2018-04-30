8ece9bebe299a7ca40e3f571c2f7e4b8460e45f1
Kurt Hetrick <khetric1@jhmi.edu>
2018-04-30 14:55:57 -0400

    bug fixes

--- scripts/A.02_LAB_PREP_METRICS.sh
+++ scripts/A.02_LAB_PREP_METRICS.sh
@@ -38,12 +38,12 @@ SAMPLE_SHEET_NAME=`basename $SAMPLE_SHEET .csv`
 # Make a QC report just for a project in the sample sheet.
 
 (head -n 1 $SAMPLE_SHEET ; \
-	awk 'BEGIN {FS=",";OFS=","} NR>1 $1=="'$PROJECT'"' {print $0} $SAMPLE_SHEET ) \
+	awk 'BEGIN {FS=",";OFS=","} $1=="'$PROJECT'" {print $0}' $SAMPLE_SHEET ) \
 	>| $CORE_PATH/$PROJECT/TEMP/$SAMPLE_SHEET_NAME"_"$START_LAB_PREP_METRICS".csv"
 
 $JAVA_1_8/java -jar $LAB_QC_DIR/EnhancedSequencingQCReport.jar \
 -lab_qc_metrics \
-$$CORE_PATH/$PROJECT/TEMP/$SAMPLE_SHEET_NAME"_"$START_LAB_PREP_METRICS".csv" \
+$CORE_PATH/$PROJECT/TEMP/$SAMPLE_SHEET_NAME"_"$START_LAB_PREP_METRICS".csv" \
 $CORE_PATH \
 $CORE_PATH/$PROJECT/TEMP/$SAMPLE_SHEET_NAME".LAB_PREP_METRICS.csv"
 
